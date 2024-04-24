2cf7bcd4e1108408ab378f81986c1b4af848bec6
10122025-Riyan Chikal.S <116341210+Riyanchikal@users.noreply.github.com>
2024-04-24 22:47:34 +0700

    add converter feature

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// ini file js
+
+let isReverse = false;
+
+function reverse() {
+    let cToF = document.getElementById('c-to-f')
+    let fToC = document.getElementById('f-to-c')
+
+   if (isReverse) {
+        console.log(isReverse);
+        fToC.style.display = "block";
+        cToF.style.display = "none";
+        isReverse = false;
+
+   } else {
+        console.log(isReverse);
+        cToF.style.display = "block";
+        fToC.style.display = "none";
+        isReverse = true;
+   }
+}
+
+function convert() {
+    let celcius = document.getElementById('c-to-f-input').value;
+    if (isNaN(celcius)) {
+        alert("tolong input nomer")
+    }
+    console.log('celcius');
+}
+
+// document.getElementById("button-convert").addEventListener('click', () => convert());
